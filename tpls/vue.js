ef828e71329f6c56af4a00e5db64b09487c44dec
黄小龙 <cdhuangxiaolong@XNMAC-C02VP4SJH.local>
2018-03-20 17:45:42 +0800

    dist 1.0.0

---
+++ tpls/vue.js
@@ -0,0 +1,50 @@
+import { mapGetters } from 'vuex';
+
+export default {
+    name: '',
+    mixins: [''],
+    props: {
+
+	},
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        ...mapGetters([])
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+    beforeCreate: function () {
+
+    },
+    created: function () {
+
+    },
+    beforeMount: function () {
+
+    },
+    mounted: function () {
+
+    },
+    beforeUpdate: function () {
+
+    },
+    updated: function () {
+
+    },
+    beforeDestroy: function () {
+
+    },
+    destroyed: function () {
+
+    },
+    watch: {
+
+    }
+}
---
+++ tpls/vue.tpl
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+const _script = require('./index');
+
+export default _script;
+</script>
+
+<style lang='scss'>
+@import './_style';
+</style>
